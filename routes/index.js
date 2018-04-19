1e8bb545b6ca470ff18b7c46b40b6813468171d6
Kelsin33 <kdmassey2@gmail.com>
2018-04-19 19:09:33 -0400

    updated files

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var geocoder = require('geocoder'); // geocoder library
 
 // our db model
 var Animal = require("../models/model.js");
 
 // simple route to render am HTML form that can POST data to our server
-// NOTE that this is not a standard API route, and is really for testing
+// NOTE that this is not a standard API route, and is really just for testing
 router.get('/create-pet', function(req,res){
-  res.render('create-pet.html')
+  res.render('pet-form.html')
+})
+
+router.get('/create-pet-location', function(req,res){
+  res.render('pet-form-with-location.html')
 })
 
 // simple route to render an HTML page that pulls data from our server and displays it on a page
@@ -24,7 +29,7 @@ router.get('/show-pets', function(req,res){
  * @return {Object} json
  */
 router.get('/', function(req, res) {
-  
+
   var jsonData = {
   	'name': 'node-express-api-boilerplate',
   	'api-status':'OK'
@@ -33,11 +38,6 @@ router.get('/', function(req, res) {
   // respond with json data
   res.json(jsonData)
 });
-
-// simple route to show an HTML page
-router.get('/sample-page', function(req,res){
-  res.render('sample.html')
-})
 
 // /**
 //  * POST '/api/create'
@@ -75,7 +75,7 @@ router.post('/api/create', function(req, res){
     var animal = new Animal(animalObj);
 
     // now, save that animal instance to the database
-    // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save    
+    // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save
     animal.save(function(err,data){
       // if err saving, respond back with error
       if (err){
@@ -92,9 +92,10 @@ router.post('/api/create', function(req, res){
         animal: data
       }
 
-      return res.json(jsonData);
+      //return res.json(jsonData);
+      return res.redirect('/show-pets')
 
-    })  
+    })
 });
 
 // /**
@@ -111,7 +112,7 @@ router.get('/api/get/:id', function(req, res){
   // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
   Animal.findById(requestedId, function(err,data){
 
-    // if err or no user found, respond with error 
+    // if err or no user found, respond with error
     if(err || data == null){
       var error = {status:'ERROR', message: 'Could not find that animal'};
        return res.json(error);
@@ -124,7 +125,7 @@ router.get('/api/get/:id', function(req, res){
     }
 
     return res.json(jsonData);
-  
+
   })
 })
 
@@ -138,18 +139,18 @@ router.get('/api/get', function(req, res){
 
   // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
   Animal.find(function(err, data){
-    // if err or no animals found, respond with error 
+    // if err or no animals found, respond with error
     if(err || data == null){
       var error = {status:'ERROR', message: 'Could not find animals'};
       return res.json(error);
     }
 
-    // otherwise, respond with the data 
+    // otherwise, respond with the data
 
     var jsonData = {
       status: 'OK',
       animals: data
-    } 
+    }
 
     res.json(jsonData);
 
@@ -170,7 +171,7 @@ router.get('/api/search', function(req,res){
 
   // let's find that animal
   Animal.find({name: searchTerm}, function(err,data){
-    // if err, respond with error 
+    // if err, respond with error
     if(err){
       var error = {status:'ERROR', message: 'Something went wrong'};
       return res.json(error);
@@ -179,17 +180,17 @@ router.get('/api/search', function(req,res){
     //if no animals, respond with no animals message
     if(data==null || data.length==0){
       var message = {status:'NO RESULTS', message: 'We couldn\'t find any results'};
-      return res.json(message);      
+      return res.json(message);
     }
 
-    // otherwise, respond with the data 
+    // otherwise, respond with the data
 
     var jsonData = {
       status: 'OK',
       animals: data
-    } 
+    }
 
-    res.json(jsonData);        
+    res.json(jsonData);
   })
 
 })
@@ -209,7 +210,7 @@ router.post('/api/update/:id', function(req, res){
    var dataToUpdate = {}; // a blank object of data to update
 
     // pull out the information from the req.body and add it to the object to update
-    var name, age, weight, color, url; 
+    var name, age, weight, color, url;
 
     // we only want to update any field if it actually is contained within the req.body
     // otherwise, leave it alone.
@@ -252,7 +253,7 @@ router.post('/api/update/:id', function(req, res){
     console.log('the data to update is ' + JSON.stringify(dataToUpdate));
 
     // now, update that animal
-    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
+    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate
     Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
       // if err saving, respond back with error
       if (err){
@@ -263,7 +264,7 @@ router.post('/api/update/:id', function(req, res){
       console.log('updated the animal!');
       console.log(data);
 
-      // now return the json data of the new person
+      // now return the json data of the new animal
       var jsonData = {
         status: 'OK',
         animal: data
@@ -304,5 +305,132 @@ router.get('/api/delete/:id', function(req, res){
   })
 
 })
+
+// examples of a GET route using an HTML template
+
+router.get('/pets', function(req,res){
+
+  Animal.find(function(err, data){
+    // if err or no animals found, respond with error
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find animals'};
+      return res.json(error);
+    }
+
+    // otherwise, respond with the data
+
+    var templateData = {
+      status: 'OK',
+      animals: data
+    }
+
+    res.render('pet-template.html',templateData);
+
+  })
+
+})
+
+module.exports = router;
+
+
+// examples of a GET route using an HTML template
+router.get('/edit/:id', function(req,res){
+
+  var requestedId = req.params.id;
+
+  // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
+  Animal.findById(requestedId, function(err,data){
+    // if err or no user found, respond with error
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find that animal'};
+       return res.json(error);
+    }
+
+    // otherwise preprate data of the animal
+    var templateData = data;
+
+    return res.render('edit-form.html', templateData);
+
+  })
+})
+
+router.post('/api/create/location', function(req, res){
+
+    console.log(req.body);
+
+    // pull out the information from the req.body
+    var name = req.body.name;
+    var age = req.body.age;
+    var tags = req.body.tags.split(","); // split string into array
+    var weight = req.body.weight;
+    var color = req.body.color;
+    var url = req.body.url;
+    var location = req.body.location;
+
+    // hold all this data in an object
+    // this object should be structured the same way as your db model
+    var animalObj = {
+      name: name,
+      age: age,
+      tags: tags,
+      description: {
+        weight: weight,
+        color: color
+      },
+      url: url
+    };
+
+  // if there is no location, return an error
+    if(!location) return res.json({status:'ERROR', message: 'You are missing a required field or have submitted a malformed request.'})
+
+    console.log('location is --> ' + location);
+    //now, let's geocode the location
+    geocoder.geocode(location, function (err,data) {
+
+      // if we get an error, or don't have any results, respond back with error
+      if (!data || data==null || err || data.status == 'ZERO_RESULTS'){
+        var error = {status:'ERROR', message: 'Error finding location'};
+        return res.json({status:'ERROR', message: 'You are missing a required field or have submitted a malformed request.'})
+      }
+
+      console.log(data);
+      // else, let's pull put the lat lon from the results
+      var lon = data.results[0].geometry.location.lng;
+      var lat = data.results[0].geometry.location.lat;
+
+      // now, let's add this to our animal object from above
+      animalObj.location = {
+        geo: [lon,lat], // need to put the geo co-ordinates in a lng-lat array for saving
+        name: data.results[0].formatted_address // the location name
+      }
+
+      // now, let's save it to the database
+      // create a new animal model instance, passing in the object we've created
+      var animal = new Animal(animalObj);
+
+      // now, save that animal instance to the database
+      // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save
+      animal.save(function(err,data){
+        // if err saving, respond back with error
+        if (err){
+          var error = {status:'ERROR', message: 'Error saving animal'};
+          return res.json(error);
+        }
+
+        console.log('saved a new animal!');
+        console.log(data);
+
+        // now return the json data of the new animal
+        var jsonData = {
+          status: 'OK',
+          animal: data
+        }
+
+        return res.json(jsonData);
+
+      })
+
+    });
+});
 
 module.exports = router;
